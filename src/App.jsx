3fc6918a494472fef236d1ea38fce61079edfd3e
diegoram23 <diego.pta.oc@gmail.com>
2023-02-17 09:15:01 -0800

    typo

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export default function App() {
 
   React.useEffect(() => {
     const allHeld = dice.every(die => die.isHeld)
-    const allEqual = dice => dice.every(die => die.value === dice[0].value)
+    const allEqual = dice.every(die => die.value === dice[0].value)
 
     if (allHeld && allEqual) {
       setTenzies(true)
